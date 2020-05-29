f1fbfa54ba3dc6f5d5633613e9f7c4ec1625f09b
Eduardo Henrique <eduardohenriquemoreirasilva@gmail.com>
2020-05-29 12:42:17 -0300

    Leitura dos dados

--- src/alteta.java
+++ src/alteta.java
@@ -7,9 +7,61 @@ public class alteta {
 		// TODO Auto-generated method stub
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+
+		boolean boleanAtleta, boleanSexo;
+		boleanAtleta = true;
+		boleanSexo = true;
+		String nome;
+		char sexo, respAtleta;
+		double altura, peso;
 		
-		
-		
+		while(boleanAtleta) {
+			 
+			 // Entrada do nome //
+			 System.out.println("Nome: ");
+			 nome = sc.nextLine();
+			 
+			// Entrada do sexo //
+			 while(boleanSexo) {
+				 System.out.println("Sexo: ");
+				 sexo = sc.next().charAt(0);
+				 sexo = Character.toLowerCase(sexo);
+				 if(sexo == 'f') {
+					 boleanSexo = false;
+				 }else if(sexo == 'm') {
+					 boleanSexo = false;
+				 }else {
+					 boleanSexo = true;
+				 }
+			 }
+			 boleanSexo = true;
+			 
+			 // Entrada da altura //
+			 System.out.println("Altura: ");
+			 altura = sc.nextDouble();
+			 while(altura < 0.0) {
+				 System.out.println("Altura precisa ser POSITIVO. Tente novamente: ");
+				 altura = sc.nextDouble();
+			 }
+			 
+			 // Entrada do peso //
+			 System.out.println("Peso: ");
+			 peso = sc.nextDouble();
+			 while(peso < 0.0) {
+				 System.out.println("Peso precisa ser POSITIVO. Tente novamente: ");
+				 peso = sc.nextDouble();
+			 }
+			 
+			 // Entrada pra saber se repete  //
+			 System.out.println("Digitar outro atleta (S/N)?");
+			 respAtleta = sc.next().charAt(0);
+			 respAtleta = Character.toLowerCase(respAtleta);
+			 if(respAtleta == 'n') {
+				 boleanAtleta = false;
+			 }
+			 
+			 sc.nextLine();
+		 }
 		sc.close();
 	}
 
